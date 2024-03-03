01f9b64271202ab113400ac4c3aa1948d251b7d0
Peter <pjb294@cornell.edu>
2024-03-03 15:07:20 -0500

    Initial Commit

---
+++ All In/All_InApp.swift
@@ -0,0 +1,17 @@
+//
+//  All_InApp.swift
+//  All In
+//
+//  Created by Peter Bidoshi  on 3/3/24.
+//
+
+import SwiftUI
+
+@main
+struct All_InApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ All In/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  All In
+//
+//  Created by Peter Bidoshi  on 3/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
