79af526fcfbbb94f81b30b8f05c9fcfbe60215e7
Antoinette Marie Torres <act238@cornell.edu>
2024-04-14 15:52:49 -0400

    Added profile view

--- All In/Views/ProfileView.swift
+++ All In/Views/ProfileView.swift
@@ -12,47 +12,70 @@ struct ProfileView: View {
     let columns = [GridItem(.flexible()), GridItem(.flexible())]
 
     var body: some View {
-
-        // Header
-        VStack(spacing: 0) {
-            ZStack(alignment: .bottomLeading) {
-                Constants.Colors.white
-                    .frame(height: 129)
-                HStack(alignment: .center) {
-                    Text("Profile")
-                        .font(.system(size: 36, weight: .bold))
-                        .foregroundStyle(.black)
-                    Spacer()
-                    HStack {
-                        Image("Money")
-                        Text("1000")
-                            .font(.system(size: 18, weight: .bold))
+        GeometryReader { geometry in
+            
+            // Header
+            VStack(spacing: 0) {
+                ZStack(alignment: .bottomLeading) {
+                    Constants.Colors.white
+                        .frame(height: 129)
+                    HStack(alignment: .center) {
+                        Text("Profile")
+                            .font(.system(size: 36, weight: .bold))
                             .foregroundStyle(.black)
+                        Spacer()
+                        HStack {
+                            Image("Money")
+                            Text("1000")
+                                .font(.system(size: 18, weight: .bold))
+                                .foregroundStyle(.black)
+                        }
+                    }
+                    .padding()
+                }
+                Divider()
+                
+                // Main frontend
+                ScrollView {
+                    ZStack {
+                        Constants.Colors.grey00
+                        
+                        VStack {
+                            HStack(alignment: .center, spacing: 28) {
+                                Image("profile-test")
+                                    .frame(width: 125, height: 125)
+                                    .background(.white)
+                                    .cornerRadius(100)
+                                    .shadow(color: .black.opacity(0.25), radius: 2)
+                                VStack(alignment: .leading) {
+                                    Text("Anton Matchev")
+                                        .font(.system(size: 24, weight: .semibold))
+                                    Text("Active Contracts: 5")
+                                        .font(.system(size: 13, weight: .regular))
+                                        .foregroundStyle(.black)
+                                    Text("Past Contracts: 10")
+                                        .font(.system(size: 13, weight: .regular))
+                                        .foregroundStyle(.black)
+                                }
+                            }
+                            .padding(EdgeInsets(top: 14, leading: 26, bottom: 8, trailing: 26))
+                            Divider()
+                                .frame(width: geometry.size.width / 1.17)
+                            LazyVGrid(columns: columns, spacing: 16) {
+                                ForEach(Contract.dummyData) { contract in
+                                    ContractCard(contract: contract)
+                                }
+                            }.padding(EdgeInsets(top: 16, leading: 16, bottom: 0, trailing: 16))
+                        }
                     }
                 }
-                .padding()
+                
+                // Tab logic
+                TabBar(page: "profile")
+                    .frame(height: 108)
             }
-            Divider()
-
-            // Main frontend
-            ScrollView {
-                Divider()
-                ZStack {
-                    Constants.Colors.grey00
-
-                    LazyVGrid(columns: columns, spacing: 16) {
-                        ForEach(Contract.dummyData) { contract in
-                            ContractCard(contract: contract)
-                        }
-                    }.padding(EdgeInsets(top: 16, leading: 16, bottom: 0, trailing: 16))
-                }
-            }
-
-            // Tab logic
-            TabBar(page: "profile")
-                .frame(height: 108)
+            .ignoresSafeArea(edges: .all)
         }
-        .ignoresSafeArea(edges: .all)
     }
 
 }
