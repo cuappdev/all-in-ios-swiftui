62c936c9fdaf45cfb0da7c43053f8302d7bafc1e
Peter <pjb294@cornell.edu>
2024-04-22 22:49:36 -0400

    update networking

--- All In/Networking/NetworkManager.swift
+++ All In/Networking/NetworkManager.swift
@@ -6,54 +6,185 @@
 //
 
 import Alamofire
+import AlamofireImage
 import CryptoKit
-import Foundation
+import SwiftUI
 
 class NetworkManager {
     static let shared = NetworkManager()
 
     private let base = "LINK GOES HERE"
 
-    private init() {
-        
-    }
+    private init() { }
+
+    // MARK: - POST create user
 
     func createUser(username: String, email: String) {
-
         let parameters = [
             "username": username,
             "email": email
         ]
 
         AF.request("\(base)/users/", method: .post, parameters: parameters, encoding: JSONEncoding.default).responseDecodable(of: User.self) { response in
-
+            switch response.result {
+            case .success(let user):
+                UserDefaults.standard.setValue(user.id, forKey: UserDefaultKeys.userID)
+            case .failure(let error):
+                print("ERROR in NetworkManager.createUser: \(error)")
+            }
         }
     }
 
-    func getUserImage(id: Int) {
+    // MARK: - GET user
 
+    func getUser(id: Int, completion: @escaping (User) -> Void) {
+        AF.request("\(base)/users/\(id)", method: .get).responseDecodable(of: User.self) { response in
+            switch response.result {
+            case .success(let user):
+                completion(user)
+            case .failure(let error):
+                print("ERROR in NetworkManager.getUser: \(error)")
+            }
+        }
     }
-}
 
-// USER - MOVE
-struct User: Decodable {
+    // MARK: - PATCH upload user image
 
-    let id: Int
-    let username: String
-    let email: String
-    let balance: Double
-    let contracts: [Contract]
-    let sellerTransations: [Transaction]
-    let buyerTransactions: [Transaction]
+    func uploadUserImage(image: UIImage, completion: @escaping (UIImage) -> Void) {
+        let id = UserDefaults.standard.integer(forKey: UserDefaultKeys.userID)
 
-}
+        AF.upload(multipartFormData: { multipartFormData in
+            if let data = image.jpegData(compressionQuality: 0.5) {
+                multipartFormData.append(data, withName: "image")
+            }
+        }, to: "\(base)/users/\(id)/image").responseImage { response in
+            switch response.result {
+            case .success(let image):
+                completion(image)
+            case .failure(let error):
+                print("ERROR in NetworkManager.uploadUserImage: \(error)")
+            }
+        }
+    }
 
-struct Transaction: Decodable {
+    // MARK: - GET user image
 
-    let id: Int
-    let transactionDate: Date
-    let price: Int
-    let sellerId: Int
-    let buyerId: Int
+    func getUserImage(completion: @escaping (UIImage) -> Void) {
+        let id = UserDefaults.standard.integer(forKey: UserDefaultKeys.userID)
+        
+        AF.request("\(base)/users/\(id)", method: .get).responseImage { response in
+            switch response.result {
+            case .success(let image):
+                completion(image)
+            case .failure(let error):
+                print("ERROR in NetworkManager.getUserImage: \(error)")
+            }
+        }
+    }
+
+    // MARK: - GET player
+
+    func getPlayer(id: Int, completion: @escaping (Player) -> Void) {
+        AF.request("\(base)/players/\(id)", method: .get).responseDecodable(of: Player.self) { response in
+            switch response.result {
+            case .success(let player):
+                completion(player)
+            case .failure(let error):
+                print("ERROR in NetworkManager.getPlayer: \(error)")
+            }
+        }
+    }
+
+    // MARK: - POST buy rarity contract from chest
+
+    func getRarity(buyPrice: Double, rarity: Rarity, completion: @escaping (Contract) -> Void) {
+        let id = UserDefaults.standard.integer(forKey: UserDefaultKeys.userID)
+        
+        let parameters = [
+            "buy_price": buyPrice,
+            "rarity": rarity.rawValue
+        ] as [String : Any]
+
+
+        AF.request("\(base)/users/\(id)/contracts", method: .post, parameters: parameters, encoding: JSONEncoding.default).responseDecodable(of: Contract.self) { response in
+            switch response.result {
+            case .success(let contract):
+                completion(contract)
+            case .failure(let error):
+                print("ERROR in NetworkManager.getRarityChest: \(error)")
+            }
+        }
+    }
+
+    // MARK: - POST buy player contract from chest
+
+    func getPlayerContract(buyPrice: Double, playerID: Int, completion: @escaping (Contract) -> Void) {
+        let id = UserDefaults.standard.integer(forKey: UserDefaultKeys.userID)
+
+        let parameters = [
+            "buy_price": buyPrice,
+        ]
+
+        AF.request("\(base)/users/\(id)/players/\(playerID)/contracts", method: .post, parameters: parameters, encoding: JSONEncoding.default).responseDecodable(of: Contract.self) { response in
+            switch response.result {
+            case .success(let contract):
+                completion(contract)
+            case .failure(let error):
+                print("ERROR in NetworkManager.getRarityChest: \(error)")
+            }
+        }
+    }
+
+    // MARK: - GET all contracts on market
+
+    func getContracts(completion: @escaping ([Contract]) -> Void) {
+        AF.request("\(base)/contracts", method: .get).responseDecodable(of: [Contract].self) { response in
+            switch response.result {
+            case .success(let contracts):
+                completion(contracts)
+            case .failure(let error):
+                print("ERROR in NetworkManager.getContracts: \(error)")
+            }
+        }
+    }
+
+    // MARK: - POST buy a contract
+
+    func buyContract(contractID: Int, completion: @escaping (Contract) -> Void) {
+        let id = UserDefaults.standard.integer(forKey: UserDefaultKeys.userID)
+
+        let parameters = [
+            "buyer_id": id
+        ]
+
+        AF.request("\(base)/contracts/\(contractID)/buy", method: .post, parameters: parameters, encoding: JSONEncoding.default).responseDecodable(of: Contract.self) { response in
+            switch response.result {
+            case .success(let contract):
+                completion(contract)
+            case .failure(let error):
+                print("ERROR in NetworkManager.buyContract: \(error)")
+            }
+        }
+    }
+
+    // MARK: - POST sell a contract
+
+    func sellContract(contractID: Int, sellPrice: Double, completion: @escaping (Contract) -> Void) {
+        let parameters = [
+            "sell_price": sellPrice
+        ]
+
+        AF.request("\(base)/contracts/\(contractID)/sell", method: .post, parameters: parameters, encoding: JSONEncoding.default).responseDecodable(of: Contract.self) { response in
+            switch response.result {
+            case .success(let contract):
+                completion(contract)
+            case .failure(let error):
+                print("ERROR in NetworkManager.sellContract: \(error)")
+            }
+        }
+    }
+
+    // MARK: - GET all contract a user owns
+
 
 }
