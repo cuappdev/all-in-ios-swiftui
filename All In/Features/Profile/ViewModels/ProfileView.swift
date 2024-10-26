31eb17e842fc53fafaff6ef21736b4e4450ab0f6
Caitlyn Jin <cj384@cornell.edu>
2024-10-26 17:04:22 -0400

    Refactored profile

--- All In/Features/Profile/ViewModels/ProfileView.swift
+++ All In/Features/Profile/ViewModels/ProfileView.swift
@@ -23,67 +23,72 @@ struct ProfileView: View {
 
     var body: some View {
         GeometryReader { geometry in
-
-            // Header
             VStack(spacing: 0) {
-                ZStack(alignment: .bottomLeading) {
-                    Constants.Colors.white
-                        .frame(height: 129)
-                    HStack(alignment: .center) {
-                        Text("Profile")
-                            .font(.system(size: 36, weight: .bold))
-                            .foregroundStyle(.black)
-                        Spacer()
-                        HStack {
-                            Image("RedMoney")
-                            Text(user.balance.withCommas())
-                                .font(.system(size: 18, weight: .bold))
-                                .foregroundStyle(.black)
-                        }
-                    }
-                    .padding()
-                }
+                header
+
                 Divider()
 
-                // Main frontend
-                ScrollView {
-                    ZStack {
-                        Constants.Colors.grey00
+                content(geometry)
 
-                        VStack {
-                            HStack(alignment: .center, spacing: 28) {
-                                Image("profile-test")
-                                    .frame(width: 125, height: 125)
-                                    .background(.white)
-                                    .cornerRadius(100)
-                                    .shadow(color: .black.opacity(0.25), radius: 2)
-                                VStack(alignment: .leading) {
-                                    Text(user.username)
-                                        .font(.system(size: 24, weight: .semibold))
-                                    Text("Active Contracts: 5 \nPast Contracts: 10")
-                                        .font(.system(size: 13, weight: .regular))
-                                        .foregroundStyle(.black)
-                                }
-                            }
-                            .padding(EdgeInsets(top: 14, leading: 26, bottom: 8, trailing: 26))
-                            Divider()
-                                .frame(width: geometry.size.width / 1.17)
-                            LazyVGrid(columns: columns, spacing: 16) {
-                                ForEach(user.contracts) { contract in
-                                    ContractCard(contract: contract)
-                                }
-                                .cornerRadius(16)
-                            }
-                            .padding(EdgeInsets(top: 16, leading: 16, bottom: 0, trailing: 16))
-                        }
-                    }
-                }
-
-                // Tab logic
                 TabBar(page: "profile")
                     .frame(height: 108)
             }
             .ignoresSafeArea(edges: .all)
+        }
+    }
+
+    private var header: some View {
+        ZStack(alignment: .bottomLeading) {
+            Constants.Colors.white
+                .frame(height: 129)
+            HStack(alignment: .center) {
+                Text("Profile")
+                    .font(.system(size: 36, weight: .bold))
+                    .foregroundStyle(.black)
+                Spacer()
+                HStack {
+                    Image("RedMoney")
+                    Text(user.balance.withCommas())
+                        .font(.system(size: 18, weight: .bold))
+                        .foregroundStyle(.black)
+                }
+            }
+            .padding()
+        }
+    }
+
+    private func content(_ geometry: GeometryProxy) -> some View {
+        ScrollView {
+            ZStack {
+                Constants.Colors.grey00
+
+                VStack {
+                    HStack(alignment: .center, spacing: 28) {
+                        Image("profile-test")
+                            .frame(width: 125, height: 125)
+                            .background(.white)
+                            .cornerRadius(100)
+                            .shadow(color: .black.opacity(0.25), radius: 2)
+                        VStack(alignment: .leading) {
+                            Text(user.username)
+                                .font(.system(size: 24, weight: .semibold))
+                            Text("Active Contracts: 5 \nPast Contracts: 10")
+                                .font(.system(size: 13, weight: .regular))
+                                .foregroundStyle(.black)
+                        }
+                    }
+                    .padding(EdgeInsets(top: 14, leading: 26, bottom: 8, trailing: 26))
+                    Divider()
+                        .frame(width: geometry.size.width / 1.17)
+                    LazyVGrid(columns: columns, spacing: 16) {
+                        ForEach(user.contracts) { contract in
+                            ContractCard(contract: contract)
+                        }
+                        .cornerRadius(16)
+                    }
+                    .padding(EdgeInsets(top: 16, leading: 16, bottom: 0, trailing: 16))
+                }
+            }
         }
     }
 
