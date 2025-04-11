83a641b1a106c6d7036b154599fee9f9819ac45a
Peter <mrpeterssyt@gmail.com>
2025-04-11 01:32:41 -0400

    jiwon 2

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
-# Create directory for Keys.xcconfig if it doesn't exist
-mkdir -p "/Volumes/workspace/repository/All In/Supporting"
-
-# Set the Google Drive file ID and destination path
-FILE_ID="1hXxXmo97NZUcOXeBDnFKDO4ZFuO8JgN-"
-DESTINATION="/Volumes/workspace/repository/All In/Supporting/Keys.xcconfig"
-
-# Download the file using curl
-curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${FILE_ID}" > /dev/null
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${FILE_ID}" -o ${DESTINATION} :antCitation[]{citations="0c1818b3-7257-40a8-b08a-db4924de8521"}
-
-echo "Keys.xcconfig file downloaded successfully"
-
-# Make sure the file is readable
-chmod 644 "${DESTINATION}"
-
-# Display success message
-echo "Build environment prepared successfully"
+echo "Downloading Secrets"
+brew install wget
+cd $CI_PRIMARY_REPOSITORY_PATH/ci_scripts
+mkdir ../ScoreSecrets
+wget -O ../Supporting/Keys.xcconfig "$KEYS"
+wget -O ../Supporting/GoogleService-Info.plist "$GOOGLE_INFO_PLIST"
