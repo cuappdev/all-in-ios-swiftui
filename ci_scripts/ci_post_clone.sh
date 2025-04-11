b211c6a4ba7119e8555a606bd97a4be8dbc9a186
Peter <mrpeterssyt@gmail.com>
2025-04-11 01:41:12 -0400

    please

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -2,7 +2,14 @@
 
 echo "Downloading Secrets"
 brew install wget
-cd $CI_PRIMARY_REPOSITORY_PATH/ci_scripts
-mkdir ../ScoreSecrets
+cd "$CI_PRIMARY_REPOSITORY_PATH/All in"
+
+if [ ! -d "../Supporting" ]; then
+    mkdir -p ../Supporting
+    echo "Created ../Supporting directory"
+else
+    echo "../Supporting directory already exists"
+fi
+
 wget -O ../Supporting/Keys.xcconfig "$KEYS"
 wget -O ../Supporting/GoogleService-Info.plist "$GOOGLE_INFO_PLIST"
